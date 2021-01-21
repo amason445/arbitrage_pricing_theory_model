b73d9b8d50c806aa64520b6d5aa6da962cfe94f5
amason445 <77219320+amason445@users.noreply.github.com>
2021-01-20 18:31:21 -0700

    Add files via upload

---
+++ APT Equity Premium/APT_model.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  9 14:28:55 2021
+
+@author: anon
+"""
+
+
+import os
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+#relative file pathing
+dirname = os.path.dirname(__file__)
+file_name = os.path.join(dirname, 'MCD_Factors.xlsx')
+file_name2 = os.path.join(dirname, 'MCD_output.xlsx')
+factors = pd.read_excel(file_name, sheet_name = 'Factors', engine = 'openpyxl')
+stocks = pd.read_excel(file_name, sheet_name = 'Stocks', engine = 'openpyxl')
+
+#compile master dataframe
+stocks['MCD_close_return'] = stocks['MCD_close'].pct_change()
+stocks['CMG_close_return'] = stocks['CMG_close'].pct_change()
+stocks['CAKE_close_return'] = stocks['CAKE_close'].pct_change()
+stocks['SHAK_close_return'] = stocks['SHAK_close'].pct_change()
+stocks['PLAY_close_return'] = stocks['PLAY_close'].pct_change()
+stock_returns = stocks[['Date','MCD_close_return','CMG_close_return','CAKE_close_return','SHAK_close_return','PLAY_close_return']]
+master_df = factors.merge(right = stock_returns, on = 'Date', how = 'left')
+
+#linear regression coefficients
+X = master_df[['SP500','CPI_food_bev', 'real_GDP', 'AAA_yield']]
+y = master_df['MCD_close_return']
+reg = LinearRegression().fit(X, y)
+betas = reg.coef_
+
+#historical means
+cmg_mean = master_df['CMG_close_return'].mean()
+cake_mean = master_df['CAKE_close_return'].mean()
+shak_mean = master_df['SHAK_close_return'].mean()
+play_mean = master_df['PLAY_close_return'].mean()
+
+#build matrixes
+dep_arrays = [betas, betas, betas, betas]
+dep_matrix = np.stack(dep_arrays)
+ind_arrays = [cmg_mean, cake_mean, shak_mean, play_mean]
+
+#solve for required rate and write output
+solutions = np.linalg.lstsq(dep_matrix,ind_arrays)[0]
+RequiredRate = (0.0049 + sum(betas * solutions))
+output = pd.DataFrame({'Required Rate of Return': [RequiredRate]})
+output.to_excel(file_name2, sheet_name = 'output', engine = 'openpyxl')
